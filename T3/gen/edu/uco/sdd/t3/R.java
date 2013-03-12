abbdd75ebe9fc7bf67c515b228c0277ac5524cca
Heru Ra <rasalatdeen@gmail.com>
2013-03-12 17:54:22 -0500

    fixed my files....

--- T3/gen/edu/uco/sdd/t3/R.java
+++ T3/gen/edu/uco/sdd/t3/R.java
@@ -27,12 +27,17 @@ public final class R {
         public static final int x_graphic=0x7f020003;
     }
     public static final class id {
+        public static final int aiVsAiButton=0x7f080027;
         public static final int boardSizes=0x7f08001d;
         public static final int five=0x7f080020;
         public static final int four=0x7f08001f;
+        public static final int hostGameButton=0x7f080025;
+        public static final int hostIp=0x7f080028;
+        public static final int joinGameButton=0x7f080029;
+        public static final int manVsManButton=0x7f080026;
         public static final int multiPlayerButton=0x7f08001c;
-        public static final int new_game=0x7f080026;
-        public static final int playGame=0x7f080025;
+        public static final int new_game=0x7f08002d;
+        public static final int playGame=0x7f08002c;
         public static final int row1col1=0x7f080000;
         public static final int row1col2=0x7f080001;
         public static final int row1col3=0x7f080002;
@@ -61,6 +66,8 @@ public final class R {
         public static final int singlePlayerButton=0x7f08001a;
         public static final int ten=0x7f080022;
         public static final int textView1=0x7f08001b;
+        public static final int textView2=0x7f08002a;
+        public static final int textView3=0x7f08002b;
         public static final int thirty=0x7f080024;
         public static final int three=0x7f08001e;
         public static final int timeoutThresholds=0x7f080021;
